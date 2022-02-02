dab3ea56209e42d91df2a310c467b8cf6a4fbd4d
Jack112-create <jackconroy777@gmail.com>
2022-02-02 18:02:10 +0000

    Add update product functionality

--- controllers/productController.js
+++ controllers/productController.js
@@ -49,6 +49,39 @@ async function createProduct(req, res) {
 
         res.writeHead(201, {'Content-Type': 'application/json'});
         return res.end(JSON.stringify(newProduct));
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// @desc    Updates a Product
+// @route   PUT /api/products
+async function updateProduct(req, res, id) {
+    try {
+
+        const product = await Product.findById(id);
+
+        if (!product) {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({message: 'Product Not Found'}));
+        } else {
+
+            const body = await getPostData(req);
+
+            const { name, description, price } = JSON.parse(body);
+    
+            const productData = {
+                name: name || product.name,
+                description: description || product.description,
+                price: price || product.price
+            }
+    
+            const updProduct = await Product.update(id, productData);
+    
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            return res.end(JSON.stringify(updProduct));
+        }
         
     } catch (error) {
         console.log(error);
@@ -58,5 +91,6 @@ async function createProduct(req, res) {
 module.exports = {
     getProducts,
     getProduct,
-    createProduct
+    createProduct,
+    updateProduct
 }
