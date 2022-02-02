4be7dc26bf3eafb5d4892bbf78564d830a973809
Jack112-create <jackconroy777@gmail.com>
2022-02-02 15:56:30 +0000

    Add functionality to find product by id

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,7 @@
 const Product = require('../models/ProductModel');
 
+// @desc    Gets All Products
+// @route   GET /api/products
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll()
@@ -10,6 +12,24 @@ async function getProducts(req, res) {
     }
 }
 
+// @route   GET /api/product/:id
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById(id);
+
+        if (!product) {
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({message: 'Product Not Found'}));
+        } else {
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify(product));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 }
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const { getProducts, getProduct } = require('./controllers/productController');
 const server = http.createServer((req, res) => {
     if (req.url == '/api/products' && req.method == "GET" ) {
         getProducts(req, res);
-    } else if (req.url.match(/\/api\/products\/([0-9] +)/) && req.method == 'GET') 
+    } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method == 'GET') 
     {
         const id = req.url.split('/')[3];
-        getProduct(req, res, id)
+        getProduct(req, res, id);
     }
     else {
         res.writeHead(404, {'Content-Type': 'application/json'});
